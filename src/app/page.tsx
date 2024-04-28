ca2a47c714262f0afea4c082c116d13210e5cc81
Joao Pedro <jpcsnoberto@gmail.com>
2024-04-27 23:05:22 -0300

    feat: basic table structure and json data formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,62 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import data from "../data/data.json";
+import dict_categoria from "../data/dict_categoria.json";
+
+import { Dropdown } from "primereact/dropdown";
+
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+
 export default function Home() {
+  const [cities, setCities] = useState(Object.keys(data));
+  const [selectedCity, setSelectedCity] = useState<string | null>(null);
+  const [years, setYears] = useState<string[]>(
+    Object.keys(data["Almadina"]).sort((a, b) => parseInt(b) - parseInt(a))
+  );
+  const [selectedYear, setSelectedYear] = useState<string | null>(null);
+  const [cityData, setCityData] = useState<any | null>({});
+
+  useEffect(() => {
+    if (selectedCity && selectedYear) {
+      let finalData = data[selectedCity][selectedYear];
+      let keys = Object.keys(finalData);
+      for (let i = 0; i < keys.length; i++) {
+        finalData[keys[i]] = Object.entries(finalData[keys[i]]).map(([key, value]) => key + ": " + value).join("\n");
+      }
+      let finalData2 = [finalData]
+
+      setCityData(finalData2 as any);
+    }
+  }, [selectedCity, selectedYear]);
+
   return (
-    <h1>TJ - EDUCAÇÃO</h1>
+    <div className="w-full max-w-[90%] my-0 mx-auto">
+      <Dropdown
+        value={selectedCity}
+        onChange={(e) => setSelectedCity(e.value)}
+        options={cities}
+        placeholder="Selecione uma cidade"
+      />
+
+      <Dropdown
+        value={selectedYear}
+        onChange={(e) => setSelectedYear(e.value)}
+        options={years}
+        placeholder="Selecione um ano"
+      />
+
+      <div>
+        <DataTable value={cityData || []} tableStyle={{ minWidth: "50rem" }}>
+          {cityData && Object.keys(cityData).length > 0 &&
+            Object.keys(cityData[0]).map((key, i) => (
+              <Column key={i} field={key} header={dict_categoria[key]?.['nome'] || key} pt={{
+                bodyCell: 'whitespace-pre-line',
+              }}/>
+            ))}
+        </DataTable>
+      </div>
+    </div>
   );
 }
